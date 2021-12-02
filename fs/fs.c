56c8e9ddc4e0991e2e23c519542479c04bf572e7
ubuntu chen <1760237725@qq.com>
2021-12-02 21:39:27 +0800

    complete exercise 3:fs/fs.c:alloc_block to modify bitmap

--- fs/fs.c
+++ fs/fs.c
@@ -62,7 +62,14 @@ alloc_block(void)
 	// super->s_nblocks blocks in the disk altogether.
 
 	// LAB 5: Your code here.
-	panic("alloc_block not implemented");
+	//search bitmap
+	for(uint32_t i = 3; i < super->s_nblocks; ++i){
+		if(block_is_free(i)){
+			bitmap[i/32] &= ~(1 << (i%32));
+			flush_block(bitmap);
+			return i;
+		}
+	}
 	return -E_NO_DISK;
 }
 
