e73ea96b33deffc7348083d32b4c3d2ff839178d
ubuntu chen <1760237725@qq.com>
2021-12-02 14:06:55 +0800

    pre edittion for lab5:exercise 1.ps:page-aligned to 4B for tf_esp in lib/spawn.c:spawn 110

--- lib/spawn.c
+++ lib/spawn.c
@@ -107,7 +107,7 @@ spawn(const char *prog, const char **argv)
 	child_tf = envs[ENVX(child)].env_tf;
 	child_tf.tf_eip = elf->e_entry;
 
-	if ((r = init_stack(child, argv, &child_tf.tf_esp)) < 0)
+	if ((r = init_stack(child, argv, ROUNDDOWN(&child_tf.tf_esp, 4))) < 0)
 		return r;
 
 	// Set up program segments as defined in ELF header.
