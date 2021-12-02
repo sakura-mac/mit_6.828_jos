1f789e436ee3fad6fb1cc1d35d1063f13ccc1eab
ubuntu chen <1760237725@qq.com>
2021-12-02 20:14:48 +0800

    complete exercise 2: modify fs/bc.c to PGFLT and flush in disk

--- fs/bc.c
+++ fs/bc.c
@@ -48,7 +48,10 @@ bc_pgfault(struct UTrapframe *utf)
 	// the disk.
 	//
 	// LAB 5: you code here:
-
+	addr = ROUNDDOWN(addr, PGSIZE);
+	sys_page_alloc(0, addr, PTE_W|PTE_U|PTE_P);
+	if((r = ide_read(blockno * BLKSECTS, addr, BLKSECTS)) < 0)
+		panic("ide_read: %e", r);
 	// Clear the dirty bit for the disk block page since we just read the
 	// block from disk
 	if ((r = sys_page_map(0, addr, 0, addr, uvpt[PGNUM(addr)] & PTE_SYSCALL)) < 0)
@@ -77,7 +80,14 @@ flush_block(void *addr)
 		panic("flush_block of bad va %08x", addr);
 
 	// LAB 5: Your code here.
-	panic("flush_block not implemented");
+	int r;
+	addr = ROUNDDOWN(addr, PGSIZE);//hint
+	if(!va_is_mapped(addr) || !va_is_dirty(addr))return;//hint
+	if((r = ide_write(blockno * BLKSECTS, addr, BLKSECTS)) < 0)
+		panic("flush_block: ide_write failed! %e\n", r);
+	if((r = sys_page_map(0,addr,0,addr, uvpt[PGNUM(addr)] & PTE_SYSCALL)) < 0)//hint
+		panic("flush_block: sys_page_map failed! %e\n",r);
+
 }
 
 // Test that the block cache works, by smashing the superblock and
